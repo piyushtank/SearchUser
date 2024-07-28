a839381a7ef0bb9bb687e7031276692b2b46f42e
Piyush Tank <piyushtank@gmail.com>
2024-07-28 13:09:12 -0700

    Added basic SwiftUI components
    Description:
    1. Added text field
    2. Added list(rows) to show the search result that contains : image, displayname, username

--- SearchUser/ContentView.swift
+++ SearchUser/ContentView.swift
@@ -5,15 +5,42 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var searchText: String = "search-user"
+    
+    var users = ["user1", "user2", "user3", "user4"]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Search users...", text: $searchText)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            List(users, id: \.self) { user in
+                HStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .background(.yellow)
+                        .foregroundStyle(.tint)
+                        .frame(width: 30, height: 30, alignment: .center)
+                        .cornerRadius(10.0)
+                    
+                    Spacer()
+                        .frame(width: 12)
+                    
+                    Text("Display Name")
+                        .font(Font.headline.weight(.bold))
+                        .lineLimit(1)
+                    
+                    Text(user)
+                        .font(Font.headline.weight(.light))
+                        .lineLimit(1)
+                }
+            }
         }
-        .padding()
+        .listStyle(PlainListStyle())
     }
 }
 
