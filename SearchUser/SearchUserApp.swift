36731626439ea8dcf5c42815388ce1756ace73ae
Piyush Tank <piyushtank@gmail.com>
2024-07-28 12:16:10 -0700

    A skeleton SwiftUI Xcode project with gitignore.
    Consists following:
    1. A skeleton project created by Xcode (15.3)
    2. Minimal .gitignore
    3. Project contains unit, ui and launch tests

---
+++ SearchUser/SearchUserApp.swift
@@ -0,0 +1,15 @@
+//
+//  SearchUserApp.swift
+//  SearchUser
+//
+
+import SwiftUI
+
+@main
+struct SearchUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
