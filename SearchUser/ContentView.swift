c7f8f2c772945eff7d649848c7f7e5a801a95bd9
Piyush Tank <piyushtank@gmail.com>
2024-07-28 13:13:51 -0700

    Wrapped the view inside a navigation view controller
    Description:
    Wrapped view body's top level VStack inside a navigation controller

--- SearchUser/ContentView.swift
+++ SearchUser/ContentView.swift
@@ -13,34 +13,37 @@ struct ContentView: View {
     var users = ["user1", "user2", "user3", "user4"]
 
     var body: some View {
-        VStack {
-            TextField("Search users...", text: $searchText)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            List(users, id: \.self) { user in
-                HStack {
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .background(.yellow)
-                        .foregroundStyle(.tint)
-                        .frame(width: 30, height: 30, alignment: .center)
-                        .cornerRadius(10.0)
-                    
-                    Spacer()
-                        .frame(width: 12)
-                    
-                    Text("Display Name")
-                        .font(Font.headline.weight(.bold))
-                        .lineLimit(1)
-                    
-                    Text(user)
-                        .font(Font.headline.weight(.light))
-                        .lineLimit(1)
+        NavigationView {
+            VStack {
+                TextField("Search users...", text: $searchText)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                List(users, id: \.self) { user in
+                    HStack {
+                        Image(systemName: "globe")
+                            .imageScale(.large)
+                            .background(.yellow)
+                            .foregroundStyle(.tint)
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .cornerRadius(10.0)
+                        
+                        Spacer()
+                            .frame(width: 12)
+                        
+                        Text("Display Name")
+                            .font(Font.headline.weight(.bold))
+                            .lineLimit(1)
+                        
+                        Text(user)
+                            .font(Font.headline.weight(.light))
+                            .lineLimit(1)
+                    }
                 }
             }
+            .listStyle(PlainListStyle())
         }
-        .listStyle(PlainListStyle())
+        .navigationTitle("Search User")
     }
 }
 
