017a0fc4526b0d5917f7eed833add7ced30fcf02
Piyush Tank <piyushtank@gmail.com>
2024-07-29 19:57:20 -0700

    Implemented StorageManager to support offline mode
    Description
    1. Storage manager implementation
    2. Removed file header name

---
+++ SearchUser/StorageManager.swift
@@ -0,0 +1,67 @@
+//
+//  StorageManager.swift
+//  SearchUser
+//
+
+import Foundation
+import UIKit
+
+class StorageManager {
+    
+    private static let usersKey = "Users"
+    private static let avatarsKey = "Avatars"
+    private static let searchesKey = "Searches"
+    
+    func saveUsers(_ users: [User], for term: String) {
+        var theUsers = UserDefaults.standard.object(forKey: StorageManager.usersKey) as? [String:Data] ?? [String:Data]()
+        var theSearches = UserDefaults.standard.object(forKey: StorageManager.searchesKey) as? [String:[String]] ?? [String:[String]]()
+
+        var ids = [String]()
+        for user in users {
+            if let data = try? JSONEncoder().encode(user) {
+                theUsers["\(user.id)"] = data
+                ids.append("\(user.id)")
+            }
+        }
+        
+        theSearches[term] = ids
+        UserDefaults.standard.setValue(theUsers, forKey: StorageManager.usersKey)
+        UserDefaults.standard.setValue(theSearches, forKey: StorageManager.searchesKey)
+    }
+    
+    func saveAvatar(_ image: UIImage, id: Int) {
+        var theAvatars = UserDefaults.standard.object(forKey: StorageManager.avatarsKey) as? [String:Data] ?? [String:Data]()
+        
+        if let data = image.jpegData(compressionQuality: 1.0) {
+            theAvatars["\(id)"] = data
+        }
+        UserDefaults.standard.setValue(theAvatars, forKey: StorageManager.avatarsKey)
+    }
+    
+    var users: [String:SearchUserResult] {
+        var results = [String:SearchUserResult]()
+        let theUsers = UserDefaults.standard.object(forKey: StorageManager.usersKey) as? [String:Data]
+        let theAvatars = UserDefaults.standard.object(forKey: StorageManager.avatarsKey) as? [String:Data]
+        
+        guard let theUsers = theUsers, let theAvatars = theAvatars else { return results }
+        
+        for (id, value) in theUsers {
+            
+            if let user = try? JSONDecoder().decode(User.self, from: value),
+               let imageData = theAvatars[id] {
+                
+                if let image = UIImage(data: imageData) {
+                    results[id] =  SearchUserResult(user: user, image: image)
+                }
+            }
+        }
+
+        return results
+    }
+    
+    var termsAndUserIds: [String:[String]] {
+        return UserDefaults.standard.object(forKey: StorageManager.searchesKey) as? [String:[String]] ?? [String:[String]]()
+    }
+
+    
+}
